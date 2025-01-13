d2bddec42e1e1c06727f735c3b9bbdb470cde5b8
clintrhodes <clint.rhodes@gmail.com>
2025-01-12 20:11:49 -0600

    Update give_kudos.py

--- give_kudos.py
+++ give_kudos.py
@@ -22,7 +22,7 @@ class KudosGiver:
         self.max_run_duration = max_run_duration
         self.start_time = time.time()
         self.num_entries = 100
-        self.web_feed_entry_pattern = '[data-testid=web-feed-entry]'
+        self.web_feed_entry_pattern = 'data-testid="web-feed-entry"'
 
         p = sync_playwright().start()
         self.browser = p.firefox.launch() # does not work in chrome
